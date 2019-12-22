bab54c28135b1de77ef4e4edde9717ef945176a9
Theroumag <40362456+Theroumag@users.noreply.github.com>
2019-12-21 22:18:28 -0800

    Update pareto.py

--- pareto.py
+++ pareto.py
@@ -1,13 +1,15 @@
 import random
 from matplotlib import pyplot as plt
 
+plt.ion()
+traders = []
+
 class Trader():
     def __init__(self, balance):
         self.balance = balance
     def __lt__(self, other):
         return self.balance < other.balance
 
-traders = []
 def generate_traders(trader_amount, trader_balance):
     for x in range(trader_amount):
         t = Trader(balance=trader_balance)
@@ -17,41 +19,27 @@ def faceoff(traders=traders):
     for trader in traders:
         if trader.balance >= 1:
             trader.balance -= 1
-            
             while True:
                 receiver = random.choice(traders)
                 if receiver.balance >= 1:
                     receiver.balance += 1
                     break
-
+                    
 def generations(repetitons):
-    for generation in range(repetitons): 
+    for generation in range(repetitons):
+        data = {}
         faceoff(traders)
+        traders.sort()
+        for t in traders:
+            if t.balance not in data.keys():
+                data[t.balance] = 1
+            else:
+                data[t.balance] += 1
+                
+        plt.cla()
+        plt.bar(data.keys(), data.values(), width=0.8, bottom=None, align='center')
+        plt.pause(0.1)
+        plt.show()
 
-x_data = []
-y_data = []
-
-def process_data():
-    global x_data
-    global y_data
-    traders.sort()
-
-    for t in traders:
-        if t.balance not in x_data:
-            x_data.append(t.balance)
-            y_data.append(1)
-        else:
-            y_data[-1] += 1
-
-def main(t, tb, gens):
-    generate_traders(trader_amount=t, trader_balance=tb)
-    generations(repetitons=gens)
-    process_data()
-    plt.bar(x_data, y_data, width=0.8, bottom=None, align='center')
-    plt.show()
-
-main(
-    t = 10,    # Traders
-    tb = 1,    # Trader Balance
-    gens = 100 # Trade rounds 
-    )
+generate_traders(trader_amount=100, trader_balance=5)
+generations(repetitons=100)
